e872fd42356c11e41704584fe6f413c7a80642a6
yitznewton <yitznewton@hotmail.com>
2020-02-14 16:53:19 -0500

    Mash volume; add Qty expectation matcher

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,52 @@
 import Qty from 'js-quantities';
 
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toEqualQty: (expected: Qty) => CustomMatcherResult;
+    }
+  }
+}
+
+expect.extend({
+  toEqualQty(actual: Qty, expected: Qty) {
+    if (actual.eq(expected)) {
+      return {
+        message: () => `${actual.toString()} should not equal ${expected.toString()}`,
+        pass: true
+      }
+    } else {
+      return {
+        message: () => `${actual.toString()} should equal ${expected.toString()}`,
+        pass: false
+      }
+    }
+  }
+});
+
+const GRAIN_DISPLACEMENT = Qty('0.32 quarts').div(Qty('1 pound'));
+
 const Mash = {
   strikeWaterVolume:
     (
       {
-       grainMass,
-       initialMashThickness
+        grainMass,
+        initialMashThickness
       }: {
         grainMass: Qty,
         initialMashThickness: MashThickness
       }
     ): Qty => (
       grainMass.mul(initialMashThickness.volume.div(initialMashThickness.mass))
+    ),
+  mashVolume:
+    (
+      args: {
+        grainMass: Qty,
+        initialMashThickness: MashThickness
+      }
+    ): Qty => (
+      args.grainMass.mul(GRAIN_DISPLACEMENT).add(Mash.strikeWaterVolume(args))
     )
 };
 
@@ -19,10 +54,24 @@ class MashThickness {
   constructor(readonly volume: Qty, readonly mass: Qty) {}
 }
 
-test('strike water volume', () => {
-  const grainMass = Qty('10 lbs');
-  const initialMashThickness = new MashThickness(Qty('1.25 quarts'), Qty('1 lb'));
-  expect(Mash.strikeWaterVolume({grainMass, initialMashThickness}).eq(Qty('12.5 quarts'))).toEqual(true);
+describe('mash', () => {
+  test('strike water volume', () => {
+    const grainMass = Qty('10 lbs');
+    const initialMashThickness = new MashThickness(Qty('1.25 quarts'), Qty('1 lb'));
+
+    const actual = Mash.strikeWaterVolume({grainMass, initialMashThickness});
+    const expected = Qty('12.5 quarts');
+    expect(actual).toEqualQty(expected);
+  });
+
+  test('mash volume', () => {
+    const grainMass = Qty('10 lbs');
+    const initialMashThickness = new MashThickness(Qty('1.25 quarts'), Qty('1 lb'));
+
+    const actual = Mash.mashVolume({grainMass, initialMashThickness});
+    const expected = Qty('15.7 quarts');
+    expect(actual).toEqualQty(expected);
+  });
 });
 
 describe('strike water temperature', () => {
